0fa600f99acfaff4af55db0a42aedbe0e96111f4
MARWANANO32 <marwanano32@gmail.com>
2024-09-20 03:44:23 +0300

    MARWAN MOSTAFA

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/user/bin/python3
+"""making change"""
+
+
+def makeChange(coins, total):
+    '''changes needed to reach total'''
+
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
